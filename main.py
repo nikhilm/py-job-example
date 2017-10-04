273f50285e2a3fe888a71bb4858111b219b91a0d
Nikhil Marathe <nsm.nikhil@gmail.com>
2017-10-03 22:25:21 -0700

    clean up some whitespace

--- main.py
+++ main.py
@@ -23,15 +23,18 @@ def parent():
     print("parent started", os.getpid())
     job = CreateJobObject(None, "my-first-job")
     AssignProcessToJobObject(job, GetCurrentProcess())
+
     for i in range(3):
         subprocess.Popen("python main.py /child")
+
     raw_input("press any key to kill all child processes:")
+
     try:
         job_processes = QueryInformationJobObject(None, JobObjectBasicProcessIdList)
         for pid in job_processes:
             if pid == os.getpid(): # Don't kill ourselves
                 continue
-            
+
             child_handle = OpenProcess(PROCESS_TERMINATE, True, pid)
             TerminateProcess(child_handle, 1)
             print("Killed", pid)
@@ -42,7 +45,7 @@ def parent():
 def child():
     print("child running", os.getpid())
     time.sleep(300)
-    
+
 if __name__ == '__main__':
     if "/child" in sys.argv:
         child()
